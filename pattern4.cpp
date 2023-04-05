76e6968b0732449ca54e5933eec09f8f7961006f
harshvijay <72139720+harshvijay63@users.noreply.github.com>
2023-04-05 12:15:48 +0530

    april5

---
+++ pattern4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cin>>n;
+    // for(int row = 0; row < n; row++)
+    // {
+    //     for ( int col = 0; col < n-row ; col++)
+    //     {
+    //         cout<<col+1;
+    //     } cout << endl;
+    // }
+
+    // double x =1.2;
+    // int sum = (int)x +1;
+    // cout<<"Sum =" <<sum;
+    // return 0;
+int a = 0;
+int b =6 ;
+ int c = 6/0;
+ cout<<c<<endl;
+}
