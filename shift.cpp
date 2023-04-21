4e22059c1858a0adc4bc60ddce8274dc0e83a990
harshvijay <72139720+harshvijay63@users.noreply.github.com>
2023-04-21 13:06:15 +0530

    left shift concept

---
+++ shift.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 12;
+    a = a << 5;
+    cout << a;
+}
+
+
+// 12 * 2 = 24        24*2 = 48        48*2 = 96          96*2 = 192       192*2= 384
