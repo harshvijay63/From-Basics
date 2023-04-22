825c1aa98924a2aafa6f4236d3bcbedc4f7df072
harshvijay <72139720+harshvijay63@users.noreply.github.com>
2023-04-22 11:24:05 +0530

    codechef

---
+++ shift1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 12;
+    a = a>> 2;
+    cout << a ;
+}
+
+
+
+
+// in right shift it will divide by 2 
+
+// 12 /2 = 6        6/2 = 3 
