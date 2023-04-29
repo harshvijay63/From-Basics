7408ab961aa4a4bed65f7acb1f2feea85ed4dd65
harshvijay <72139720+harshvijay63@users.noreply.github.com>
2023-04-29 15:11:30 +0530

    increment

---
+++ increment.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    cout << a++ <<endl; // output is 5  because it gives a value then it goes to increment
+    cout<<++a<<endl; // output is 6      because it gives a value because it increment first then then value
+
+}
